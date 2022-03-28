0e2eac03e2516cbbb0da99d41dc22042149a6650
Alberto Zugazagoitia <alberto@zugazagoitia.com>
2022-03-28 20:03:34 +0200

    Update paths and version

--- src/main/java/com/zugazagoitia/knag/users/UserRepository.java
+++ src/main/java/com/zugazagoitia/knag/users/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+@RepositoryRestResource(collectionResourceRel = "users", path = "v1/users")
 public interface UserRepository extends MongoRepository<User, String> {
 
 	List<User> findByName(@Param("name") String name);
