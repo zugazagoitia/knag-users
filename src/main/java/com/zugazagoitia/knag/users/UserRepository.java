dda830abc96b191265cae006cae5807838be4075
Alberto Zugazagoitia <alberto@zugazagoitia.com>
2022-03-29 14:32:37 +0200

    Hotfix invalid path
    Bump to 0.1.3

--- src/main/java/com/zugazagoitia/knag/users/UserRepository.java
+++ src/main/java/com/zugazagoitia/knag/users/UserRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "users", path = "v1/users")
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends MongoRepository<User, String> {
 
 	List<User> findByName(@Param("name") String name);
